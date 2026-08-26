5a727f8c8a1c3268c59da59fc3ad637f9de6cb5a
Eli Heady <eli.heady@gmail.com>
2026-08-26 15:51:36 -0400

    PR commenting script
    This pulls the PR commenting steps out into a reusable script
    called from both the DNSControl check and the main DNSControl steps
    
    also added some comments to explain the potentially confusing DEBUG=true
    in the Debug Helpers step

---
+++ bin/comment.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+set -eo pipefail
+
+# this script is called by the dnscontrol-action commenting steps. it is not
+# expected to work outside of GHA
+
+$FJECHO "$STEP_TITLE"
+comment_id=""
+page=1
+while [[ -z "$comment_id" ]]; do
+  if [[ -n "$API_PAGER" ]]; then
+    query_string="?${API_PAGER}=$PAGE_LIMIT&page=$page"
+  else
+    query_string=""
+  fi
+  $DEBUG "fetching page $page of PR comments:"
+  $DEBUG "${PR_API_URL}${query_string}"
+
+  comments=$($CURLJSON -H "Authorization: Token ${API_TOKEN}" \
+    "${PR_API_URL}${query_string}")
+  comment_id=$(printf '%s' "$comments" \
+    | jq -r '[.[] | select(.body | contains("${MARKER}")) | .id] | first // empty')
+
+  comments_length=$(printf '%s' "$comments" | jq 'length')
+  $DEBUG "API returned $comments_length comments"
+
+  if [[ -z "$API_PAGER" ]] || \
+     [[ $comments_length -lt $PAGE_LIMIT ]] || \
+     [[ -n "$comment_id" ]]; then
+    break
+  fi
+  ((page++))
+done
+
+comment_payload=$(jq -n --arg body "$COMMENT_BODY" '{body: $body}')
+
+if [[ -n "$comment_id" ]]; then
+  $DEBUG "updating comment $comment_id"
+  comment=$($CURLJSON -X PATCH -H "Authorization: Token ${API_TOKEN}" --json "$comment_payload" \
+    "${COMMENT_API_URL}/$comment_id")
+else
+  $DEBUG "posting new comment"
+  comment=$($CURLJSON -X POST -H "Authorization: Token ${API_TOKEN}" --json "$comment_payload" \
+    "${PR_API_URL}")
+fi
+$DEBUG "$comment"
